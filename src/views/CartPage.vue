9634b7fef8e95505e8a17026a8a53b6322908837
moonlightboss <cumrak.legenda@gmail.com>
2023-03-05 13:33:44 +0600

    Added files

---
+++ src/views/CartPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="page-wrap">
+    <h1>Shopping Cart</h1>
+    <ProductsList
+      :products="cartItems"
+      v-on:remove-from-cart="removeFromCart($event)"/>
+    <h3 id="total-price">Total: ${{ totalPrice }}</h3>
+    <button id="checkout-button">Proceed to Checkout</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import ProductsList from '../components/ProductsList.vue';
+
+export default {
+    name: 'CartPage',
+    components: {
+      ProductsList,
+    },
+    data() {
+      return {
+        cartItems: [],
+      }
+    },
+    computed: {
+      totalPrice() {
+        return this.cartItems.reduce(
+          (sum, item) => sum + Number(item.price),
+          0,
+        );
+      }
+    },
+    methods:{
+      async removeFromCart(productId){
+        const result = await axios.delete(`/api/users/12345/cart/${productId}`)
+        this.cartItems = result.data;
+      }
+    },
+    async created(){
+      const result = await axios.get('/api/users/12345/cart')
+      const cartItems = result.data;
+      this.cartItems = cartItems;
+    },
+};
+</script>
+
+<style scoped>
+  h1 {
+    border-bottom: 1px solid black;
+    margin: 0;
+    margin-top: 16px;
+    padding: 16px;
+  }
+
+  #total-price {
+    padding: 16px;
+    text-align: right;
+  }
+
+  #checkout-button {
+    width: 100%;
+  }
+</style>
---
+++ src/views/ProductsPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="page-wrap">
+      <products-grid :products="products" />
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import ProductsGrid from '@/components/ProductsGrid.vue';
+export default {
+  components: { ProductsGrid },
+    name:'ProductsPage',
+    data(){
+        return{
+            products: [],
+        };
+    },
+    async created(){
+      const result = await axios.get('/api/products');
+      const products = result.data;
+      this.products = products;
+    },
+};
+</script>
+
+<style scoped>
+</style>
